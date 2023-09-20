aa6c3dee31de7ef593741033e4d751545f244427
Dileep Kumar <77086107+Dileepgithub91@users.noreply.github.com>
2023-09-21 00:06:36 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize("sequelize_forum", "root", "Dileep91@", {
   dialect: "mysql",
-  host:"localhost",
+  host:"127.0.0.1",
   port:3306,
 });
 
